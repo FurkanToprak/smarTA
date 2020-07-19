c5e009c4c52a9c0ab03daee425c5269a5ae9629c
Furkan Toprak <furkancemaltoprak@gmail.com>
2020-07-18 20:57:38 -0500

    inc

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,12 @@
-/* eslint-disable no-undef */
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
-app.listen(port, err => {
-  if (err) {
-    return console.error(err);
-  }
+app.listen(port, () => {
   return console.log(`server is listening on ${port}`);
 });
